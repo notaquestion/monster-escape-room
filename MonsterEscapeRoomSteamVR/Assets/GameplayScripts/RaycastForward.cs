c07f70830c7d5cde0074b0b0d0c744d433893ea3
Brandae Grein <brandae.grein@bellevuecollege.edu>
2018-09-15 16:00:17 -0700

    -Jason about to start working

---
+++ MonsterEscapeRoomSteamVR/Assets/GameplayScripts/RaycastForward.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RaycastForward : MonoBehaviour {
+    // private Valve.VR.InteractionSystem.Hand hand;
+    //bool isTriggerPressed;
+    GameObject currentObject = null;
+    public void pullObject()
+    {
+
+        Debug.Log("You did something");
+        
+    }
+    void Start()
+    {
+        //hand = gameObject.GetComponent<Valve.VR.InteractionSystem.Hand>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        RaycastHit hit;
+        float theDistance;
+
+        //debug raycast in the editor
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+        if (Physics.Raycast(transform.position, (forward), out hit, 100f, 1 << 8))
+        {
+
+            theDistance = hit.distance;
+            Debug.Log(theDistance + " meters away");
+            currentObject = hit.transform.gameObject;
+        }
+        else
+        {
+            currentObject = null;
+        }
+        // SteamVR_Controller.Input
+    }
+}
---
+++ MonsterEscapeRoomSteamVR/Assets/Scripts/ChairExplosion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairExplosion : MonoBehaviour {
+    /*
+    GameObject ParentGameObject = this.gameObject;
+    GameObject chairPart1 = ParentGameObject.transform.GetChild(0).gameObject;
+    GameObject chairPart2 = ParentGameObject.transform.GetChild(1).gameObject;
+    GameObject chairPart3 = ParentGameObject.transform.GetChild(0).gameObject;
+    GameObject chairPart4 = ParentGameObject.transform.GetChild(1).gameObject;
+    // Use this for initialization
+    public void chairExplodes()
+    {
+        chairPart1
+    }
+    
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		*/
+	
+}
