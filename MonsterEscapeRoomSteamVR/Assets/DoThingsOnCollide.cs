dcad8f34afc65b79673625b24a8d018201ab748c
Brandae Grein <brandae.grein@bellevuecollege.edu>
2018-09-16 00:49:44 -0700

    -Continued progress, workign door.

---
+++ MonsterEscapeRoomSteamVR/Assets/DoThingsOnCollide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DoThingsOnCollide : MonoBehaviour {
+
+    public string OtherObject;
+
+    public UnityEvent OnCollideWithObject; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == OtherObject)
+        {
+            OnCollideWithObject.Invoke();
+        }
+    }
+}
