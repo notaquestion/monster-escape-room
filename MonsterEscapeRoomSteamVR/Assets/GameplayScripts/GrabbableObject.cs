8b7e24948f6effcbf143861e7f1a9cd2f336baee
April Allderdice <april.public@gmail.com>
2018-09-15 17:47:36 -0700

    added quickOutline to highlight objects and Grabbable object script and Interactable object script

--- MonsterEscapeRoomSteamVR/Assets/GameplayScripts/GrabbableObject.cs
+++ MonsterEscapeRoomSteamVR/Assets/GameplayScripts/GrabbableObject.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GrabbableObject : MonoBehaviour {
+    //puts glow around object when selected
+    public bool breaksOnImpact;
+    public bool grabbableObject;
+    
+    public void ObjectSelected () {
+       
+       var outline = gameObject.AddComponent<Outline>();
+        outline.OutlineMode = Outline.Mode.OutlineAll;
+        outline.OutlineColor = Color.yellow;
+        outline.OutlineWidth = 9.8f;
+    }
+	public void ObjectDeselected()
+    {
+        var outline = gameObject.AddComponent<Outline>();
+        outline.OutlineMode = Outline.Mode.OutlineHidden;
+        
+    }
+    public void Reset()
+    {
+        var outline = gameObject.AddComponent<Outline>();
+        outline.OutlineMode = Outline.Mode.OutlineHidden;
+        outline.OutlineColor = Color.yellow;
+        outline.OutlineWidth = 9.8f;
+    }
+    public void Awake()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+
 }
--- MonsterEscapeRoomSteamVR/Assets/GameplayScripts/InteractableObject.cs
+++ MonsterEscapeRoomSteamVR/Assets/GameplayScripts/InteractableObject.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //puts different glow that grabbableObject when selected
+   
+    public void ObjectSelected()
+    {
+
+        var outline = gameObject.AddComponent<Outline>();
+        outline.OutlineMode = Outline.Mode.OutlineAll;
+        outline.OutlineColor = Color.green;
+        outline.OutlineWidth = 9.8f;
+    }
+    public void ObjectDeselected()
+    {
+        var outline = gameObject.AddComponent<Outline>();
+        outline.OutlineMode = Outline.Mode.OutlineHidden;
+    }
+    public void Reset()
+    {
+        var outline = gameObject.AddComponent<Outline>();
+        outline.OutlineMode = Outline.Mode.OutlineHidden;
+        outline.OutlineColor = Color.yellow;
+        outline.OutlineWidth = 9.8f;
+    }
+    public void Awake()
+    {
+        
+    }
+
 }
