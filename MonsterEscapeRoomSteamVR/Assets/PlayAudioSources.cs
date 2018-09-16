2647c291381f8759dc18987c4e7df9f1fea8838f
Brandae Grein <brandae.grein@bellevuecollege.edu>
2018-09-16 03:35:04 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ MonsterEscapeRoomSteamVR/Assets/PlayAudioSources.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioSources : MonoBehaviour {
+    public List<AudioSource> AudioSources;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaySources(int i)
+    {
+        if (AudioSources.Count > i)
+            AudioSources[i].Play();
+    }
+}
